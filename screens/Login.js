170da1323e54c6e6e03b8d09ab367c536249bb77
“Ismail <brkicismail@gmail.com>
2020-04-26 17:43:17 +0200

    Basic Login UI and move to Register from Login

--- screens/Login.js
+++ screens/Login.js
@@ -1,8 +1,16 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 class Login extends React.Component {
+  state = {
+    emailInput: "",
+    passwordInput: "",
+  };
+
   render() {
+    const { navigation } = this.props;
+
     return (
       <View
         style={{
@@ -12,10 +20,53 @@ class Login extends React.Component {
           justifyContent: "center",
         }}
       >
-        <Text>Login screen</Text>
+        <View style={{ width: 300 }}>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: "gray",
+              borderWidth: 1,
+              paddingHorizontal: 10,
+            }}
+            onChangeText={(text) => this.setState({ emailInput: text })}
+            value={this.state.emailInput}
+          />
+          <View style={{ alignItems: "center" }}>
+            <Text>Email</Text>
+          </View>
+          <View style={{ marginTop: 33 }}>
+            <TextInput
+              secureTextEntry={true}
+              style={{
+                height: 40,
+                borderColor: "gray",
+                borderWidth: 1,
+                paddingHorizontal: 10,
+              }}
+              onChangeText={(text) => this.setState({ passwordInput: text })}
+              value={this.state.passwordInput}
+            />
+            <View style={{ alignItems: "center" }}>
+              <Text>Password</Text>
+            </View>
+          </View>
+          <View style={{ width: "77%", alignSelf: "center", marginTop: 27 }}>
+            <Button title="LOGIN" />
+          </View>
+          <View style={{ width: "77%", alignSelf: "center", marginTop: 7 }}>
+            <Button
+              onPress={() => navigation.navigate("Register")}
+              title="REGISTER"
+            />
+          </View>
+        </View>
       </View>
     );
   }
 }
 
-export default Login;
+export default function (props) {
+  const navigation = useNavigation();
+
+  return <Login {...props} navigation={navigation} />;
+}
--- screens/Register.js
+++ screens/Register.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 class Register extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, width: "100%", backgroundColor: green }}>
+      <View style={{ flex: 1, width: "100%", backgroundColor: "green" }}>
         <Text>Register screen</Text>
       </View>
     );
